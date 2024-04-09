eb7a25f2414622cd0792b309aaf1dd48a197a3d2
김경호 <ykm989@naver.com>
2024-04-09 10:10:10 +0900

    [init] 프로젝트 생성

---
+++ JukoLock/JukoLock/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  JukoLock
+//
+//  Created by 김경호 on 4/9/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ JukoLock/JukoLock/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  JukoLock
+//
+//  Created by 김경호 on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+}
