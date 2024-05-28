90f63c3c8ed3cd3f430884365b2b9d9551a4b79a
김경호 <ykm989@naver.com>
2024-05-28 16:12:31 +0900

    [feat] SceneDelegate 화면 전환 코드 구현

--- JukoLock/JukoLock/App/SceneDelegate.swift
+++ JukoLock/JukoLock/App/SceneDelegate.swift
@@ -5,23 +5,53 @@
 //  Created by 김경호 on 4/9/24.
 //
 
+import Combine
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
+    private var cancellables = Set<AnyCancellable>()
+    private var loginStateSubject = CurrentValueSubject<LoginState, Never>(.loogedOut)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
-        let viewController = TabBarController()
-
-        window.rootViewController = viewController
-        window.makeKeyAndVisible()
+        bind()
     }
 }
 
+// MARK: - Methods
+extension SceneDelegate {
+    func switchViewController(for loginState: LoginState) {
+        switch loginState {
+        case .loggedIn:
+            let tabBarViewController = TabBarController()
+            self.navigationController = UINavigationController(rootViewController: tabBarViewController)
+            self.window?.rootViewController = self.navigationController
+        case .loogedOut:
+            let provider = APIProvider(session: URLSession.shared)
+            let useCase = LoginUseCase(provider: provider)
+            let viewModel = LoginViewModel(loginUseCase: useCase)
+            let loginViewController = LoginViewController(viewModel: viewModel, loginStateSubject: loginStateSubject)
+            self.navigationController = UINavigationController(rootViewController: loginViewController)
+            self.window?.rootViewController = self.navigationController
+        }
+        
+        self.window?.makeKeyAndVisible()
+    }
+}
+
+private extension SceneDelegate {
+    
+    func bind() {
+        loginStateSubject
+            .sink { [weak self] isLoginState in
+                self?.switchViewController(for: isLoginState)
+            }
+            .store(in: &cancellables)
+    }
+}
